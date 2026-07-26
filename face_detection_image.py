070f59f38b3c2d27b777413d6c3041322eb1f357
pangeran-droid <pangeranxyz80@gmail.com>
2026-07-26 20:18:40 +0700

    update

--- face_detection_image.py
+++ face_detection_image.py
@@ -3,14 +3,14 @@ import cv2
 # Read image
 img = cv2.imread('image/faces.jpeg')
 
-# Convert the image to grayscale
+# Convert image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Load the Haar cascade classifier for face detection
 face_cascade_path = 'haarcascade/haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
-# Detect faces in the grayscale image
+# Face detection
 faces = face_cascade.detectMultiScale(
     gray,
     scaleFactor=1.1,
@@ -28,7 +28,7 @@ for (x, y, w, h) in faces:
 cv2.imwrite("output/face_detection_result.jpg", img)
 print("Image saved!")
 
-# Display the result in a window
+# Display result
 cv2.imshow("Face Detection", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
